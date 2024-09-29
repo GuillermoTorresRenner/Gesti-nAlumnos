4b782ee6c79b6e5409c56d64d1417c28121296b7
LeBateleur <torresrennerguillermo@gmail.com>
2024-09-28 23:55:33 -0300

    Finalización del entegable

--- src/test/java/AlumnoServicioTest.java
+++ src/test/java/AlumnoServicioTest.java
@@ -1,67 +1,65 @@
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import modelos.*;
+import servicios.*;
 
-import modelos.Calculator;
 
-class AlumnoServicioTest {
 
-	static Calculator calculator;
-	@Test
-	//@BeforeEach // Se ejecuta antes de cada test
-	//@AfterEach // Se ejecuta después de cada test
-	//@BeforeAll // Se ejecuta antes de todos los test
-	//@AfterAll // Se ejecuta después de todos los test
-	//todos estos métodos deben ser estáticos
-	@BeforeAll
-	static void setup() {
-		calculator = new Calculator();
-	}
-	@Test
-	@DisplayName("Test add method")
-	void testAdd() {
-		double result = calculator.add(10, 50);
-		assertEquals(60, result);
-	}
-	
-	@Test
-	@DisplayName("Test subtract method")
-	void testSubtract() {
-		double result = calculator.subtract(50, 10);
-		assertEquals(40, result);
-	}
-	
-	@Test
-	@DisplayName("Test multiply method")
-	void testMultiply() {
-		double result = calculator.multiply(10, 50);
-		assertEquals(500, result);
-	}
-	
-	@Test
-	@DisplayName("Test divide method")
-	void testDivide() {
-		double result = calculator.divide(50, 10);
-		assertEquals(5, result);
-	}	
-	@Test
-	@DisplayName("Test divide by zero")
-	void testDivideByZero() {
-		assertThrows(ArithmeticException.class, () -> calculator.divide(50, 0));
-	}
-	
-	@Test
-	@DisplayName("Multiple assertions")
-	
-	void testMultipleAssertions() {
-		assertAll(
-				() -> assertEquals(60, calculator.add(10, 50)), 
-				() -> assertEquals(40, calculator.subtract(50, 10)),
-				() -> assertEquals(500, calculator.multiply(10, 50)), 
-				() -> assertEquals(5, calculator.divide(50, 10))
-				);
-	}
+public class AlumnoServicioTest {
 
-}
+    private static AlumnoServicio alumnoServicio;
+    private static AlumnoServicio alumnoServicioMock;
+    private static Materia matematicas;
+    private static Materia lenguaje;
+    private static Alumno mapu;
+
+    @BeforeAll
+    public static void setup() {
+		alumnoServicio = new AlumnoServicio();
+		alumnoServicioMock=mock(AlumnoServicio.class);
+		matematicas = new Materia(MateriaEnum.MATEMATICAS);
+		lenguaje = new Materia(MateriaEnum.LENGUAJE);
+		mapu = new Alumno("12.345.678-9", "Homero", "Simpson", "Avenida Siempreviva 743");
+
+
+    }
+
+    @DisplayName("Test para verificar la creación de un alumno")
+    @Test
+    public void crearAlumnoTest() {
+    	        alumnoServicio.crearAlumno(mapu);
+    	        assertEquals(mapu, alumnoServicio.listarAlumnos().get("12.345.678-9"));
+    }
+
+    @DisplayName("Test para verificar la agregación de una materia a un alumno")
+    @Test
+    public void agregarMateriaTest() {
+        alumnoServicio.crearAlumno(mapu);
+		alumnoServicio.agregarMateria("12.345.678-9", matematicas);
+		alumnoServicio.agregarMateria("12.345.678-9", lenguaje);
+		assertEquals(2, alumnoServicio.listarAlumnos().get("12.345.678-9").getMaterias().size());
+		
+    }
+
+    @DisplayName("Test para verificar la funcionalidad de materiasPorAlumno (usando mock)")
+    @Test
+    public void materiasPorAlumnosTest() {
+            when(alumnoServicioMock.materiasPorAlumno("12.345.678-9")).thenReturn(Arrays.asList(matematicas, lenguaje));
+    	}
+    
+    
+    @DisplayName("Test para verificar la funcionalidad de listarAlumnos")
+    @Test
+    public void listarAlumnosTest() {
+            alumnoServicio.crearAlumno(mapu);
+            assertEquals(1, alumnoServicio.listarAlumnos().size());
+        }
+    
+    }
